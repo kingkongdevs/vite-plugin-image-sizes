e1bf7f2f565afa7f00461d5f07117fb0db6fce9a
Cara Gammon <caragammon@gmail.com>
2023-09-06 16:30:18 +1000

    🚧 make it so that images only process on build

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ const { parse, HTMLElement } = require('node-html-parser');
 const processedImages = new Set();
 
 module.exports = (options) => {
-  const outputDir = options.outputDir || 'dist/assets/images'; // Set the image output directory
-  const inputDir = options.inputDir || 'src/assets/images'; // Set the image input directory
-
   return {
     name: 'vite-plugin-images',
-    async transformIndexHtml(html, { path: indexPath }) {
+
+    configResolved(resolvedConfig) {
+      // store the resolved config
+      config = resolvedConfig
+    },
+
+    async transformIndexHtml(html, { path: indexPath, context }) {
+      const imgOutputDir = options.outputDir || 'dist/assets/images'; //  Set the image output directory
+      const imgInputDir = options.imgInputDir || 'src/assets/images'; // Set the image input directory
+      const configCommand = config.command; // build or serve
+      const buildDir = config.build.outDir; // ./dist/ by default
+      
+
       return new Promise(async (resolve, reject) => {
+
         // Parse the HTML content.
         const root = parse(html);
 
@@ -29,70 +39,73 @@ module.exports = (options) => {
 
           if (src && /\.(jpg|png)$/.test(src)) {
             // Construct the full path to the input image based on /src/assets/images
-            const inputImagePath = path.resolve(inputDir, src);
+            const inputImagePath = path.resolve(imgInputDir, src);
 
             console.log(`Processing image: ${inputImagePath}`);
 
             // Check if the input image file exists.
             if (fs.existsSync(inputImagePath)) {
+              // Process the image if it is build, otherwise just use the original image inside the picture tag
+              if(configCommand === 'build') {
               // Check if the image has already been processed
-              if (!processedImages.has(inputImagePath)) {
-                // Add the input image path to the processed images Set
-                processedImages.add(inputImagePath);
+                if (!processedImages.has(inputImagePath)) {
+                  // Add the input image path to the processed images Set
+                  processedImages.add(inputImagePath);
 
-                // Get the dimensions of the original image
-                const imageMetadata = await sharp(inputImagePath).metadata();
-                const originalWidth = imageMetadata.width || 0;
-                const originalHeight = imageMetadata.height || 0;
+                  // Get the dimensions of the original image
+                  const imageMetadata = await sharp(inputImagePath).metadata();
+                  const originalWidth = imageMetadata.width || 0;
+                  const originalHeight = imageMetadata.height || 0;
 
-                // Copy the original image to the output directory
-                const outputImageCopyPath = path.resolve(outputDir, src);
-                await fs.copy(inputImagePath, outputImageCopyPath);
-                console.log(`Copied original image to: ${outputImageCopyPath}`);
+                  // Copy the original image to the output directory
+                  const outputImageCopyPath = path.resolve(imgOutputDir, src);
+                  await fs.copy(inputImagePath, outputImageCopyPath);
+                  console.log(`Copied original image to: ${outputImageCopyPath}`);
 
-                // Process the image (resize and convert to webp) for sizes smaller than the original
-                const image = sharp(inputImagePath);
-                const sizes = [320, 640, 1024];
-                const imagePromises = sizes.map(async (size) => {
-                  if (size <= originalWidth) {
-                    // Check if the output image is smaller than the size it is set to output
-                    if (image.metadata().width < size || image.metadata().height < size) {
-                      // No processing needed, return the original path
-                      return;
+                  // Process the image (resize and convert to webp) for sizes smaller than the original
+                  const image = sharp(inputImagePath);
+                  const sizes = [320, 640, 1024];
+                  const imagePromises = sizes.map(async (size) => {
+                    if (size <= originalWidth) {
+                      // Check if the output image is smaller than the size it is set to output
+                      if (image.metadata().width < size || image.metadata().height < size) {
+                        // No processing needed, return the original path
+                        return;
+                      }
+                      // Otherwise resize and output the webp format
+                      console.log(`Resizing image to ${size}px: ${inputImagePath}`);
+                      const webpBuffer = await image.clone().resize(size).toFormat('webp').toBuffer();
+                      const webpFileName = `${path.basename(src, path.extname(src))}-${size}px.webp`;
+
+                      // Specify the output directory and file path
+                      const outputImagePath = path.resolve(imgOutputDir, webpFileName);
+
+                      // Ensure that parent directories are created if they don't exist.
+                      await fs.ensureDir(path.dirname(outputImagePath));
+
+                      await fs.outputFile(outputImagePath, webpBuffer);
+                      console.log(`Generated WebP image: ${outputImagePath}`);
+
+                      // Create the html element for <source> for each image
+                      const pictureSource = new HTMLElement('source', {});
+                      pictureSource.setAttribute('srcset', `${outputImagePath} ${size}w`);
+                      pictureSource.setAttribute('type', 'image/webp');
+                      // Add the picture source elements to the img tag
+                      imgTag.appendChild(pictureSource);
                     }
-                    // Otherwise resize and output the webp format
-                    console.log(`Resizing image to ${size}px: ${inputImagePath}`);
-                    const webpBuffer = await image.clone().resize(size).toFormat('webp').toBuffer();
-                    const webpFileName = `${path.basename(src, path.extname(src))}-${size}px.webp`;
+                  });
 
-                    // Specify the output directory and file path
-                    const outputImagePath = path.resolve(outputDir, webpFileName);
-
-                    // Ensure that parent directories are created if they don't exist.
-                    await fs.ensureDir(path.dirname(outputImagePath));
-
-                    await fs.outputFile(outputImagePath, webpBuffer);
-                    console.log(`Generated WebP image: ${outputImagePath}`);
-
-                    // Create the html element for <source> for each image
-                    const pictureSource = new HTMLElement('source', {});
-                    pictureSource.setAttribute('srcset', `${outputImagePath} ${size}w`);
-                    pictureSource.setAttribute('type', 'image/webp');
-                    // Add the picture source elements to the img tag
-                    imgTag.appendChild(pictureSource);
-                  }
-                });
-
-                imageProcessingPromises.push(...imagePromises);
-              } else {
-                console.log(`Image already processed: ${inputImagePath}`);
+                  imageProcessingPromises.push(...imagePromises);
+                } else {
+                  console.log(`Image already processed: ${inputImagePath}`);
+                }
               }
 
               // Calculate the relative path from the HTML file to the new image in /dist/
-              const relativeImagePath = path.relative(path.dirname(indexPath), path.join('/', outputDir, src));
+              const relativeImagePath = path.relative(path.dirname(indexPath), path.join('/', imgOutputDir, src));
 
               // Remove the /dist/ folder from the relative path
-              const cleanRelativeImagePath = relativeImagePath.replace('dist/', '');
+              const cleanRelativeImagePath = relativeImagePath.replace('dist/', ''); // TODO: replace this with something that doesnt assume your output folder is dist
 
               console.log('Updated image src:', cleanRelativeImagePath);
 
